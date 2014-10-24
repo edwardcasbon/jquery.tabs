7730362cacccec6cf5933185fdd1a5a9afe0ddf8
Edward Casbon <edward@edwardcasbon.co.uk>
2014-10-24 12:13:18 +0100

    Added options for templating elements

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -23,87 +23,122 @@
 *
 **/
 window.tabs = (function($){
-	
+
 	// Tabs elements.
 	var tabs = [];
-	
+
 	// Global plugin settings.
 	var settings = {
-		activeClass: 			"active",
-		containerClass: 		"container",
-		tabClass: 				"tab",
 		animationSpeed: 		180,
 		scrollTo: 				true,
 		scrollToOffset: 		0,
 		pagination: 			true,
 		reloadAjax: 			false,
 		cacheAjax: 				false,
-		ajaxContainerClass: 	"ajax-container"
-	}; 
-	
+		template: {
+			container: {
+				atts: {},
+				classes: ["container"]
+			},
+			nav: {
+				links: {
+					activeClass: "active"
+				}
+			},
+			tab: {
+				container: {
+					classes: ["tab"]
+				},
+				ajaxContainer: {
+					classes: ["ajax-container"]
+				}
+			},
+			pagination: {
+				container: {
+					atts: {},
+					classes: []
+				},
+				links: {
+					prev: {
+						atts: {},
+						classes: [],
+						preHtml: "",
+						postHtml: ""
+					},
+					next: {
+						atts: {},
+						classes: [],
+						preHtml: "",
+						postHtml: ""
+					}
+				}
+			}
+		}
+	};
+
 	// IE8 check.
 	var isIE8 = function() {
 		// In IE8 positioned elements don't fade. We need to add filter:inherit css to rectify.
 		return (window.navigator.userAgent.match("IE 8")) ? true : false;
 	};
-	
+
 	// Set up the tabs.
 	var initTabs = function(options){
-		
+
 		// Merge user options into settings.
-		$.extend(settings, options);
-		
+		$.extend(true, settings, options);
+
 		// Loop through the tabbed elements.
 		return this.each(function(){
 			// Add this tabbed element to the tabs stack.
 			tabs.push(this);
-			
+
 			// Set up variables.
 			var $this = $(this),
-				$first = $this.find("." + settings.tabClass).first(),
+				$first = $this.find("." + settings.template.tab.container.classes.join(".")).first(),
 				tabType = $first.get(0).nodeName,
 				randomString = generateRandomString(4),
 				counter = 1;
-				
+
 			// Add empty containers for the ajax'ed tabs, if any.
 			$this.find("nav a").each(function(){
 				var $tabNav = $(this),
-					$lastTab = $this.find("." + settings.tabClass).last();
-					
+					$lastTab = $this.find("." + settings.template.tab.container.classes.join(".")).last();
+
 				if($tabNav.attr("href").charAt(0) !== "#") {
 					// Not an internal link, must be ajaxed.
 					var id = $tabNav.attr("data-tab-id") || "tab-" + randomString + "-" + counter, // Unique ID for the tab.
-						$container = $("<" + tabType + "/>").html("<div class=\"" + settings.ajaxContainerClass + "\"></div>").attr("id", id).addClass(settings.tabClass);
+						$container = $("<" + tabType + "/>").html("<div class=\"" + settings.template.tab.ajaxContainer.classes.join(" ") + "\"></div>").attr("id", id).addClass(settings.template.tab.container.classes.join(" "));
 					counter++;
 					$tabNav.attr("data-tab-id", id);
 					$lastTab.after($container);
 				}
 			});
-				
-			// Wrap all the tabs in a container			
-			$this.find("." + settings.tabClass).wrapAll($("<div class=\"" + settings.containerClass + "\"/>")
+
+			// Wrap all the tabs in a container
+			$this.find("." + settings.template.tab.container.classes.join(".")).wrapAll($("<div class=\"" + settings.template.container.classes.join(" ") + "\"/>")
 								/* .css("overflow", "hidden") */
 								.css("height", $first.outerHeight(true))
 								.css("clear", "both"));
-								
-			var $container = $this.find("." + settings.containerClass).first();
-					
+
+			var $container = $this.find("." + settings.template.container.classes.join(".")).first();
+
 			// Hide all tabs and show the first.
-			$this.find("." + settings.tabClass).hide().first().show();
-			
+			$this.find("." + settings.template.tab.container.classes.join(".")).hide().first().show();
+
 			// Set active tab.
-			$this.find("nav a").first().addClass(settings.activeClass);
-			
+			$this.find("nav a").first().addClass(settings.template.nav.links.activeClass);
+
 			// IE8 bug fix
 			if(isIE8()) {
 				$this.find("*").css("filter", "inherit");
 			}
-			
+
 			// Add listener.
 			$this.find("nav a").on("click", function(e){
 				var $this = $(this);
 
-				if(!$this.hasClass(settings.activeClass)) {
+				if(!$this.hasClass(settings.template.nav.links.activeClass)) {
 					if($this.attr("href").charAt(0) !== "#") {
 						e.preventDefault(); // Stop the loading of the new page.
 						window.location.hash = $this.attr("data-tab-id"); // Update the hash to the ajax tab ID.
@@ -112,87 +147,54 @@ window.tabs = (function($){
 					// Stop re-loading of current tab.
 					e.preventDefault();
 				}
-			});	
-			
+			});
+
 			// Add pagination.
 			if(settings.pagination) {
-				var tabCount = $this.find("." + settings.tabClass).length,
-					$navigation = $this.find("nav");
-				
-				$this.find("." + settings.tabClass).each(function(index){
-					var $tab = $(this),
-						$pagination = $("<nav/>").addClass("pagination"),
-						$navItem,
-						prevUrl, nextUrl;
-					
-					if(index !== 0) {
-						// Prev link
-						$navItem = $navigation.find("a").eq(index-1);
-						prevUrl = ($navItem.attr("href").charAt(0) === "#") ? $navItem.attr("href") : "#" + $navItem.attr("data-tab-id");
-						var $prevLink = $("<a/>")
-										.addClass("prev")
-										.attr("href", prevUrl)
-										.html($navItem.html());
-						$prevLink.appendTo($pagination);
-					}
-					
-					if(index !== (tabCount-1)) {
-						// Next link
-						$navItem = $navigation.find("a").eq(index+1);
-						nextUrl = ($navItem.attr("href").charAt(0) === "#") ? $navItem.attr("href") : "#" + $navItem.attr("data-tab-id");
-						var $nextLink = $("<a/>")
-										.addClass("next")
-										.attr("href", nextUrl)
-										.html($navItem.html());
-						$nextLink.appendTo($pagination);
-					}
-				
-					// Append the pagination to the article.
-					$tab.append($pagination);
-				});
+				buildPagination($this);
 			}
-			
+
 			// Recalculate the height after the window has loaded.
 			$(window).load(function(){
-				$container.css("height", $this.find("." + settings.tabClass).first().outerHeight(true));
+				$container.css("height", $this.find("." + settings.template.tab.container.classes.join(".")).first().outerHeight(true));
 			});
 		});
 	};
-	
+
 	// Switch to a tab.
 	var toggleTab = function(hash){
-		$.each(tabs, function(){	
+		$.each(tabs, function(){
 			var $this = $(this); // $this = the tabbed element containing the tabs.
-			
-			if(($this.find(hash).length > 0) && ($this.find(hash).is("." + settings.tabClass))) {
-				var $container 		= $this.find("." + settings.containerClass),
-					$activeTabNav	= $this.find("nav ." + settings.activeClass),
-					$activeTab 		= $this.find("." + settings.tabClass + ":visible"),
+
+			if(($this.find(hash).length > 0) && ($this.find(hash).is("." + settings.template.tab.container.classes.join(".")))) {
+				var $container 		= $this.find("." + settings.template.container.classes.join(".")),
+					$activeTabNav	= $this.find("nav ." + settings.template.nav.links.activeClass),
+					$activeTab 		= $this.find("." + settings.template.tab.container.classes.join(".") + ":visible"),
 					$thisTabNav		= ($this.find("nav").first().find("a[href=\"" + hash + "\"]").length !== 0) ? $this.find("nav").first().find("a[href=\"" + hash + "\"]") : $this.find("nav a[data-tab-id=\"" + hash.replace("#", "") + "\"]"),
 					$thisTab 		= $this.find(hash);
-				
+
 				// Scroll to the correct location on the page.
 				if(settings.scrollTo) {
 					$("html, body").animate({
 						scrollTop: $this.position().top + parseInt(settings.scrollToOffset)
 					});
 				}
-							
+
 				// Reset tabs
-				$activeTabNav.removeClass(settings.activeClass);
-				$thisTabNav.addClass(settings.activeClass);
-			
+				$activeTabNav.removeClass(settings.template.nav.links.activeClass);
+				$thisTabNav.addClass(settings.template.nav.links.activeClass);
+
 				// Fade out current article
 				$activeTab.fadeOut(settings.animationSpeed, function(){
-				
+
 					// If Ajax, load in content.
 					if($thisTabNav.attr("href").charAt(0) !== "#") {
-						if(settings.reloadAjax || ($thisTab.find("." + settings.ajaxContainerClass).html() === "")) {
+						if(settings.reloadAjax || ($thisTab.find("." + settings.template.tab.ajaxContainer.classes.join(".")).html() === "")) {
 							$.ajax({
 								url: $thisTabNav.attr("href"),
 								cache: settings.cacheAjax,
 								success: function(data) {
-									$thisTab.find("." + settings.ajaxContainerClass).html(data);
+									$thisTab.find("." + settings.template.tab.ajaxContainer.classes.join(".")).html(data);
 								},
 								error: function(request, status, error) {
 									window.console.log("Error: " + error);
@@ -200,26 +202,30 @@ window.tabs = (function($){
 							});
 						}
 					}
-				
+
 					// Hide next article (opacity 0)
-					$thisTab.fadeTo(settings.animationSpeed, 0, function(){
-						$thisTab.show();
-					
-						// Animate height difference on container
-						$container.animate({
-							height: $thisTab.outerHeight(true) + "px"
-						}, {
-							complete: function(){
-								$thisTab.fadeTo(settings.animationSpeed, 1);
-							},
-							duration: settings.animationSpeed
-						});
+					$thisTab.css({
+						'filter': 'alpha(opacity=0)',
+						'zoom': '1',
+						'opacity': '0'
 					});
+					$thisTab.show();
+
+					// Animate height difference on container
+					$container.animate({
+						height: $thisTab.outerHeight(true) + "px"
+					}, {
+						complete: function(){
+							$thisTab.fadeTo(settings.animationSpeed, 1);
+						},
+						duration: settings.animationSpeed
+					});
+
 				});
 			}
 		});
 	};
-	
+
 	var generateRandomString = function(length) {
 		var string = "",
 			possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -227,16 +233,60 @@ window.tabs = (function($){
 		for (var i=0; i<length; i++) {
 			string += possible.charAt(Math.floor(Math.random() * possible.length));
 		}
-		
+
 		return string;
 	};
-	
-	// Return an object.	
+
+	var buildPagination = function($container) {
+		var tabCount = $container.find("." + settings.template.tab.container.classes.join(".")).length,
+			$navigation = $container.find("nav");
+
+		$container.find("." + settings.template.tab.container.classes.join(".")).each(function(index){
+			var $tab = $(this),
+				$pagination = $("<nav/>").addClass(settings.template.pagination.container.classes.join(" ")),
+				$navItem,
+				prevUrl, nextUrl;
+
+			// Add pagination container attributes.
+			for(var att in settings.template.pagination.container.atts) {
+				$pagination.attr(att, settings.template.pagination.container.atts[att]);
+			}
+
+			if(index !== 0) {
+				// Prev link
+				$navItem = $navigation.find("a").eq(index-1);
+				prevUrl = ($navItem.attr("href").charAt(0) === "#") ? $navItem.attr("href") : "#" + $navItem.attr("data-tab-id");
+				var $prevLink = $("<a/>")
+								.addClass("prev")
+								.addClass(settings.template.pagination.links.prev.classes.join(" "))
+								.attr("href", prevUrl)
+								.html(settings.template.pagination.links.prev.preHtml + $navItem.html() + settings.template.pagination.links.prev.postHtml);
+				$prevLink.appendTo($pagination);
+			}
+
+			if(index !== (tabCount-1)) {
+				// Next link
+				$navItem = $navigation.find("a").eq(index+1);
+				nextUrl = ($navItem.attr("href").charAt(0) === "#") ? $navItem.attr("href") : "#" + $navItem.attr("data-tab-id");
+				var $nextLink = $("<a/>")
+								.addClass("next")
+								.addClass(settings.template.pagination.links.next.classes.join(" "))
+								.attr("href", nextUrl)
+								.html(settings.template.pagination.links.next.preHtml + $navItem.html() + settings.template.pagination.links.next.postHtml);
+				$nextLink.appendTo($pagination);
+			}
+
+			// Append the pagination to the article.
+			$tab.append($pagination);
+		});
+	};
+
+	// Return an object.
 	return {
 		initTabs: initTabs,
 		toggleTab: toggleTab
 	};
-	
+
 })(jQuery);
 
 // Init the tabs functionality.
